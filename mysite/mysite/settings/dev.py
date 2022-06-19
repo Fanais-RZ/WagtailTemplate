3226e3cd700311d3d00da302551485ccccfbd1ef
FR2 <38023531+Fanais-RZ@users.noreply.github.com>
2022-06-19 03:41:16 -0400

    Primer Commit
    Carga Inicial de todo el sitio web en wagtail.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6p+k_g!%@@g0x^wh)-citwj)v#v%cn4=yy4=-n$=k=x79_s#ro'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
